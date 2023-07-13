bf3004e5e17930c69c9a1c8396ac1b3f07ba6c09
fauzan <fmuharam25@gmail.com>
2023-07-13 13:28:12 +0700

    initial commit

---
+++ routes/media.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const isBase64 = require('is-base64');
+const base64Img = require('base64-img');
+
+const { Media } = require('../models');
+
+router.get('/',async(req,res)=>{
+  const media =await Media.findAll();
+  const mappedMedia =media.map((m)=>{
+    m.image=`${req.get('host')}/${m.image}`;
+    return m
+  });
+  
+  return res.json({
+    status:'success',
+    data:media
+  });
+})
+/* GET users listing. */
+router.post('/', (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: 'error', message: 'invalid base64' });
+  }
+
+  base64Img.img(image, './public/images', Date.now(),async (err, filepath) => {
+    if (err) {
+      return res.status(400).json({
+        status: 'error',
+        mesage: err.mesage
+      })
+    }
+    const filename = filepath.split('/').pop();
+    const media = await Media.create({image:`images/${filename}`});
+
+    return res.json({
+      status:'succes',
+      data:{
+        id:media.id,
+        image: `${req.get('host')}/images/${filename}`
+      }
+    })
+  });
+
+  
+});
+
+module.exports = router;
